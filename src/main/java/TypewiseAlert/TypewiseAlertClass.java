715dfd233d6b1cc3297341af24c6731834b6c481
reshmababu1896 <reshmababu1896@gmail.com>
2024-08-19 19:44:45 +0530

    Add files via upload

---
+++ src/main/java/TypewiseAlert/TypewiseAlertClass.java
@@ -0,0 +1,36 @@
+package TypewiseAlert;
+
+import TypewiseAlert.BreachClassifier.BreachType;
+import TypewiseAlert.BreachClassifier.CoolingType;
+import TypewiseAlert.AlertSender.AlertTarget;
+
+public class TypewiseAlertClass {
+
+    public static class BatteryCharacter {
+        private CoolingType coolingType;
+        private String brand;
+
+        public BatteryCharacter(CoolingType coolingType, String brand) {
+            this.coolingType = coolingType;
+            this.brand = brand;
+        }
+
+        public CoolingType getCoolingType() {
+            return coolingType;
+        }
+
+        public String getBrand() {
+            return brand;
+        }
+    }
+
+    public static void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+        BreachType breachType = BreachClassifier.classifyTemperatureBreach(batteryChar.getCoolingType(), temperatureInC);
+
+        if (alertTarget == AlertTarget.TO_CONTROLLER) {
+            AlertSender.sendToController(breachType);
+        } else if (alertTarget == AlertTarget.TO_EMAIL) {
+            AlertSender.sendToEmail(breachType);
+        }
+    }
+}
