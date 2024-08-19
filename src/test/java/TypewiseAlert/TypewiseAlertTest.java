9e812d7232bffa07da641487f6776d2fe2aaa96b
reshmababu1896 <reshmababu1896@gmail.com>
2024-08-19 20:05:46 +0530

    Update TypewiseAlertTest.java

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import TypewiseAlert.BreachClassifier.BreachType;
 import TypewiseAlert.BreachClassifier.CoolingType;
 import TypewiseAlert.AlertSender.AlertTarget;
+import TypewiseAlert.TypewiseAlert.BatteryCharacter;
 
 public class TypewiseAlertTest {
 
